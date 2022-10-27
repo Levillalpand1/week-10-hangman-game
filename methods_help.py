967d63a7f46734860086f93167e605d0b50e435f
Luis Villalpando <113930826+Levillalpand1@users.noreply.github.com>
2022-10-27 16:58:48 +0000

    I finished challenge 1 and 2

--- methods_help.py
+++ methods_help.py
@@ -45,7 +45,6 @@ def method_help():
 
 
 
-
 ########################################################################################################################
   #   Methods, Help & Documentation Practice #3
   # Check if the sets below are isolated (that is, they have no elements in common), using the isdisjoint() method. Store this result in the isolated_sets variable:
@@ -53,6 +52,6 @@ def method_help():
   phone_brands = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"}
   tv_brands = {"Sony", "Philips", "Samsung", "LG"}
   
-  isolated_sets= phone_brands.isdisjoint([])
+  isolated_sets= phone_brands.isdisjoint(tv_brands)
 
   # Search the documentation for the requested method to know how it works.
